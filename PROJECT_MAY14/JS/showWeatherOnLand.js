c0ece585099d750f31ee4084ecc98f25a1a5c783
HabilOezcan <habil.oezcan@powercoders.com>
2021-05-14 20:02:14 +0200

    icon addresess redefined

--- PROJECT_MAY14/JS/showWeatherOnLand.js
+++ PROJECT_MAY14/JS/showWeatherOnLand.js
@@ -12,11 +12,11 @@ const showWeatherOnLand = (pData)=>{
             <li>Observation Time : ${pData.ob_time} GMT</li>
             <li>City Name : ${pData.city_name}</li>
             <li>Temprature : ${pData.temp} &deg;C</li>
-            <li><img src='../IMGs/icons/c01d.png' alt='sunrise'width='15' height='15'>${pData.sunrise} GMT</li>
-            <li><img src='../IMGs/icons/sunset1.jpg' alt='sunrise'width='15' height='15'>${pData.sunset} GMT</li>
+            <li><img src='/IMGs/icons/c01d.png' alt='sunrise'width='15' height='15'>${pData.sunrise} GMT</li>
+            <li><img src='/IMGs/icons/sunset1.jpg' alt='sunset'width='15' height='15'>${pData.sunset} GMT</li>
             <li>Wind Speed : ${pData.wind_spd} m/sn</li>
             <li>Wind Direction : ${pData.wind_dir}</li>
-            <li><img src='../IMGs/icons/${pData.weather.icon}.png' alt='sunrise'width='15' height='15'> ${pData.weather.description}</li>
+            <li><img src='/IMGs/icons/${pData.weather.icon}.png' alt='weather definition'width='15' height='15'> ${pData.weather.description}</li>
         </ul>
     `;
     weatherContent.innerHTML = content;
